c890ae732e2daf4623ba87aa81364f04047c6ec3
James Yarbro <yarbro@outlook.com>
2016-09-05 08:41:19 +0200

    Added a new Forum3 base project using the RTM .net web project template. Added CodeKicker.BBCode source.

---
+++ CodeKicker.BBCode/BBCodeParsingException.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeKicker.BBCode {
+	[Serializable]
+	public class BBCodeParsingException : Exception {
+		public BBCodeParsingException() {
+		}
+		public BBCodeParsingException(string message)
+			: base(message) {
+		}
+	}
+}
