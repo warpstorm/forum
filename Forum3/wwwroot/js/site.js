113f80073f75aa6a617ebec6848b67abae1cf1a8
James Yarbro <yarbro@outlook.com>
2016-11-29 14:27:36 +0100

    Finally finished migrating over files from the intermediate project I used to migrate from netcore RC to netcore 1.0

--- Forum3/wwwroot/js/site.js
+++ Forum3/wwwroot/js/site.js
@@ -1,1 +1,18 @@
-﻿// Write your Javascript code.
+﻿$(function () {
+    // TODO - enable shift-click and middle click to new windows
+    $("[clickableLinkParent]").on("click", function (e) {
+        e.stopPropagation();
+        window.location.href = $(this).find("a").eq(0).attr("href");
+    });
+
+    $("#createTopicButton").on("click", function (e) {
+        OpenModal($(e).data("source"));
+    });
+});
+
+function OpenModal(url) {
+    if (url && url.length > 0) {
+        $("#modal").html("");
+        $("#modal").load(url);
+    }
+}
