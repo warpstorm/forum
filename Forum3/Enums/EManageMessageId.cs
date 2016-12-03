bb6b5594a31ca4edadc8d459ec21187cbb805831
James Yarbro <yarbro@outlook.com>
2016-12-03 11:51:46 +0100

    minor

--- Forum3/Enums/EManageMessageId.cs
+++ Forum3/Enums/EManageMessageId.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Forum3.Enums
-{
+﻿namespace Forum3.Enums {
 	public enum ManageMessageId {
 		AddPhoneSuccess,
 		AddLoginSuccess,
