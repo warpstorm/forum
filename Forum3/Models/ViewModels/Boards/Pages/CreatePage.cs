efa0da0f7e8a24a8c722885aeaa89b6bcadcdbad
James Yarbro <yarbro@outlook.com>
2017-05-12 22:44:44 +0200

    Started working on the board create page.

--- Forum3/Models/ViewModels/Boards/Pages/CreatePage.cs
+++ Forum3/Models/ViewModels/Boards/Pages/CreatePage.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 namespace Forum3.Models.ViewModels.Boards.Pages {
 	public class CreatePage {
 		public string Name { get; set; }
+		public string Category { get; set; }
 		public bool VettedOnly { get; set; }
 		public List<SelectListItem> Categories { get; set; }
 	}
