a2a69465542479948b4508e72182e1024b4593c0
Jim Yarbro <yarbro@outlook.com>
2017-11-04 11:55:37 +0100

    removed unnecessary property

--- Forum3/Models/DataModels/Category.cs
+++ Forum3/Models/DataModels/Category.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Forum3.Models.DataModels {
 	public class Category {
@@ -10,7 +9,5 @@ namespace Forum3.Models.DataModels {
 		public string Name { get; set; }
 
 		public int DisplayOrder { get; set; }
-
-		public List<Board> Boards { get; set; }
 	}
 }
