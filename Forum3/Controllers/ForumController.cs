68257165cb861bde003db8410e756af2b47fae0d
Jim Yarbro <yarbro@outlook.com>
2018-01-15 16:53:16 +0100

    Better error page. Check app configuration if migration is allowed.

--- Forum3/Controllers/ForumController.cs
+++ Forum3/Controllers/ForumController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ namespace Forum3.Controllers {
 	using ViewModels = Models.ViewModels;
 
 	public class ForumController : Controller {
+		[AllowAnonymous]
 		public IActionResult Error() => View(new ViewModels.Error { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 
 		public string Referrer {
--- Forum3/Migrator/AddMigratorExtension.cs
+++ Forum3/Migrator/AddMigratorExtension.cs
@@ -4,6 +4,11 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Forum3.Migrator {
 	public static class AddMigratorExtension {
 		public static IServiceCollection AddMigrator(this IServiceCollection services, IConfiguration configuration) {
+			var allowMigration = configuration["AllowMigration"];
+
+			if (string.IsNullOrEmpty(allowMigration) || allowMigration != "true")
+				return services;
+
 			services.AddScoped((serviceProvider) => {
 				var connectionString = configuration["Version2Connection"];
 				return new Models.MigratorDbContext(connectionString);
