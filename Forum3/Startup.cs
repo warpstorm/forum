76768a02df27617d48302c155032fe6dabc49cce
Jim Yarbro <yarbro@outlook.com>
2018-04-08 20:59:31 +0200

    forcing error pages for now.

--- Forum3/Startup.cs
+++ Forum3/Startup.cs
@@ -74,8 +74,10 @@ namespace Forum3 {
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+			app.UseDeveloperExceptionPage();
+
 			if (env.IsDevelopment()) {
-				app.UseDeveloperExceptionPage();
+				//app.UseDeveloperExceptionPage();
 				app.UseBrowserLink();
 				app.UseDatabaseErrorPage();
 			}
