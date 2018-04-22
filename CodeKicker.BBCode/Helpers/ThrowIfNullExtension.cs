5c93bb2e619149258796375d16406b7d6a06f54d
Jim Yarbro <yarbro@outlook.com>
2018-04-22 17:19:35 +0200

    Made extension method internal to prevent accidental coupling.

--- CodeKicker.BBCode/Helpers/ThrowIfNullExtension.cs
+++ CodeKicker.BBCode/Helpers/ThrowIfNullExtension.cs
@@ -2,7 +2,7 @@
 
 namespace CodeKicker.BBCode.Helpers {
 	/// Source: https://stackoverflow.com/questions/11522104
-	public static class ThrowIfNullExtension {
+	internal static class ThrowIfNullExtension {
 		public static void ThrowIfNull<T>(this T o, string paramName, bool allowEmptyStrings = false) where T : class {
 			if (o is null)
 				throw new ArgumentNullException(paramName);
