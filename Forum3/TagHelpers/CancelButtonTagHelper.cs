68f9fff5362eb52f51e8075cbe4bdb5539b1619f
Jim Yarbro <yarbro@outlook.com>
2018-04-23 13:00:54 +0200

    Cancel button tag helper.

---
+++ Forum3/TagHelpers/CancelButtonTagHelper.cs
@@ -0,0 +1,48 @@
+﻿using Forum3.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Forum3.TagHelpers {
+	public class CancelButtonTagHelper : TagHelper {
+		HttpContext HttpContext { get; }
+		IUrlHelper UrlHelper { get; }
+
+		public CancelButtonTagHelper(
+			IActionContextAccessor actionContextAccessor,
+			IUrlHelperFactory urlHelperFactory
+		) {
+			HttpContext = actionContextAccessor.ActionContext.HttpContext;
+			UrlHelper = urlHelperFactory.GetUrlHelper(actionContextAccessor.ActionContext);
+		}
+
+		public override void Process(TagHelperContext context, TagHelperOutput output) {
+			output.TagName = "a";
+
+			if (!output.Attributes.ContainsName("class"))
+				output.Attributes.SetAttribute("class", "button");
+
+			var returnUrl = GetReturnUrl();
+			output.Attributes.SetAttribute("href", returnUrl);
+
+			if (string.IsNullOrEmpty(output.Content.GetContent()))
+				output.Content.SetContent("Cancel");
+
+			output.TagMode = TagMode.StartTagAndEndTag;
+		}
+
+		string GetReturnUrl() {
+			HttpContext.Request.Query.TryGetValue("ReturnUrl", out var returnUrl);
+
+			if (string.IsNullOrEmpty(returnUrl))
+				HttpContext.Request.Query.TryGetValue("Referer", out returnUrl);
+
+			if (string.IsNullOrEmpty(returnUrl))
+				returnUrl = UrlHelper.Action(nameof(Home.FrontPage), nameof(Home));
+
+			return returnUrl;
+		}
+	}
+}
