6203c22abd7b96dd7d1b8f4b12733e6e85df93ef
Jim Yarbro <yarbro@outlook.com>
2018-10-14 23:22:34 +0200

    Support for environment appsettings.

--- Forum3/Program.cs
+++ Forum3/Program.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Forum3 {
 	public class Program {
 		public static void Main(string[] args) => BuildWebHost(args).Run();
 
-		public static IWebHost BuildWebHost(string[] args) =>
-			WebHost.CreateDefaultBuilder(args)
+		public static IWebHost BuildWebHost(string[] args) {
+			return WebHost.CreateDefaultBuilder(args)
 				.UseStartup<Startup>()
+				.ConfigureAppConfiguration((builderContext, config) => {
+					config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+						  .AddJsonFile($"appsettings.{builderContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+				})
 				.Build();
+		}
 	}
 }
