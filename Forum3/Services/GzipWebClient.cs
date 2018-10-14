5e75dbf395952028ad729c7c91708342356303e1
Jim Yarbro <yarbro@outlook.com>
2018-10-14 20:53:48 +0200

    Added DownloadJSObject method and reorganized some code for reuse.

--- Forum3/Services/GzipWebClient.cs
+++ Forum3/Services/GzipWebClient.cs
@@ -1,34 +1,63 @@
-﻿using HtmlAgilityPack;
+﻿using Forum3.Extensions;
+using HtmlAgilityPack;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Net;
 
 namespace Forum3.Services {
+	using ServiceModels = Models.ServiceModels;
+
 	public class GzipWebClient : WebClient {
 		public HtmlDocument DownloadDocument(string remoteUrl) {
-			var siteWithoutHash = remoteUrl.Split('#')[0];
-
-			var document = new HtmlDocument();
+			var data = GetRemoteData(remoteUrl);
 
 			try {
-				var data = DownloadString(siteWithoutHash);
-
-				if (string.IsNullOrEmpty(data))
-					return null;
-
-				document.LoadHtml(data);
+				data = DownloadString(remoteUrl);
 			}
 			catch (UriFormatException) { }
 			catch (AggregateException) { }
 			catch (ArgumentException) { }
 			catch (WebException) { }
 
-			return document;
+			HtmlDocument returnObject = null;
+
+			if (!string.IsNullOrEmpty(data)) {
+				returnObject = new HtmlDocument();
+				returnObject.LoadHtml(data);
+			}
+
+			return returnObject;
 		}
 
-		protected override WebRequest GetWebRequest(Uri address) {
+		public T DownloadJSObject<T>(string remoteUrl, Dictionary<HttpRequestHeader, string> headers = null) {
+			remoteUrl = CleanUrl(remoteUrl);
+
+			foreach (var header in headers)
+				Headers.Set(header.Key, header.Value);
+
+			var returnObject = default(T);
+			var data = string.Empty;
+			
+			try {
+				data = DownloadString(remoteUrl);
+			}
+			catch (UriFormatException) { }
+			catch (AggregateException) { }
+			catch (ArgumentException) { }
+			catch (WebException) { }
+
+			if (!string.IsNullOrEmpty(data))
+				returnObject = JsonConvert.DeserializeObject<T>(data);
+
+			return returnObject;
+		}
+
+		protected override WebRequest GetWebRequest(Uri remoteUri) {
 			ServicePointManager.SecurityProtocol = SecurityProtocolType.SystemDefault | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
 
-			var request = base.GetWebRequest(address) as HttpWebRequest;
+			var request = base.GetWebRequest(remoteUri) as HttpWebRequest;
 
 			request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246";
 			request.AllowAutoRedirect = true;
@@ -38,6 +67,27 @@ namespace Forum3.Services {
 			request.CookieContainer = new CookieContainer();
 
 			return request;
+		}
+
+		string CleanUrl(string remoteUrl) {
+			remoteUrl.ThrowIfNull(nameof(remoteUrl));
+			return remoteUrl.Split('#')[0];
+		}
+
+		string GetRemoteData(string remoteUrl) {
+			remoteUrl = CleanUrl(remoteUrl);
+
+			var data = string.Empty;
+
+			try {
+				data = DownloadString(remoteUrl);
+			}
+			catch (UriFormatException) { }
+			catch (AggregateException) { }
+			catch (ArgumentException) { }
+			catch (WebException) { }
+
+			return data;
 		}
 	}
 }
