b2970a7713999035020cbd6b46dd7987d81993e7
Jim Yarbro <yarbro@outlook.com>
2018-11-03 17:27:50 +0100

    migrated easter-egg

--- Forum3/client/scripts/easter-egg.ts
+++ Forum3/client/scripts/easter-egg.ts
@@ -1,9 +1,18 @@
-﻿$(function () {
-	$("#easter-egg").on("mouseenter", function () {
-		$("#danger-sign").removeClass("hidden");
-	});
+﻿export default function () {
+	var easterEgg = new EasterEgg();
+	easterEgg.addEasterEggListener();
+}
 
-	$("#easter-egg").on("mouseleave", function () {
-		$("#danger-sign").addClass("hidden");
-	});
-});
+export class EasterEgg {
+	addEasterEggListener(): void {
+		let element = document.getElementById('easter-egg');
+
+		element.addEventListener("mouseenter", function () {
+			document.getElementById("danger-sign").classList.remove("hidden");
+		});
+
+		element.addEventListener("mouseleave", function () {
+			document.getElementById("danger-sign").classList.add("hidden");
+		});
+	}
+}
