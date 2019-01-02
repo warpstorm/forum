8c6a9b972f75d2b54a71b66c6c1ef72001507c38
Jim Yarbro <yarbro@outlook.com>
2019-01-02 08:51:39 +0100

    Limit the number of requests the whos-online-monitor can make.

--- Forum/client/app/services/whos-online-monitor.ts
+++ Forum/client/app/services/whos-online-monitor.ts
@@ -6,6 +6,8 @@ import { Xhr } from "./xhr";
 import { XhrOptions } from "../models/xhr-options";
 
 export class WhosOnlineMonitor {
+	private recentRequest: boolean = false;
+
 	constructor(private doc: Document, private app: App) {
 		throwIfNull(doc, 'doc');
 		throwIfNull(app, 'app');
@@ -26,32 +28,38 @@ export class WhosOnlineMonitor {
 	}
 
 	hubWhosOnline = () => {
-		console.log("whos online event");
-
 		let self = this;
 
-		let requestOptions = new XhrOptions({
-			method: HttpMethod.Get,
-			url: '/Home/WhosOnline',
-			responseType: 'document'
-		});
+		if (!self.recentRequest) {
+			self.recentRequest = true;
 
-		Xhr.request(requestOptions)
-			.then((xhrResult) => {
-				let resultDocument = <HTMLElement>(<Document>xhrResult.response).documentElement;
-				let resultBody = <HTMLBodyElement>resultDocument.querySelector('body');
-				let resultBodyElements = resultBody.childNodes;
-				let targetElement = <Element>self.doc.querySelector(`div[sidebar="whos-online"]`);
+			let requestOptions = new XhrOptions({
+				method: HttpMethod.Get,
+				url: '/Home/WhosOnline',
+				responseType: 'document'
+			});
 
-				resultBodyElements.forEach(node => {
-					let element = node as Element;
+			Xhr.request(requestOptions)
+				.then((xhrResult) => {
+					let resultDocument = <HTMLElement>(<Document>xhrResult.response).documentElement;
+					let resultBody = <HTMLBodyElement>resultDocument.querySelector('body');
+					let resultBodyElements = resultBody.childNodes;
+					let targetElement = <Element>self.doc.querySelector(`div[sidebar="whos-online"]`);
 
-					if (element && element.tagName && element.tagName.toLowerCase() == 'div') {
-						targetElement.after(element);
-						targetElement.remove();
-					}
-				});
-			})
-			.catch(Xhr.logRejected);
+					resultBodyElements.forEach(node => {
+						let element = node as Element;
+
+						if (element && element.tagName && element.tagName.toLowerCase() == 'div') {
+							targetElement.after(element);
+							targetElement.remove();
+						}
+					});
+				})
+				.catch(Xhr.logRejected);
+						
+			setTimeout(() => {
+				self.recentRequest = false;
+			}, 10000);
+		}
 	}
 }
