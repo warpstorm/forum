d257c3f5048f7fe1b8cb76c2d031742735fb9a6e
Jim Yarbro <yarbro@outlook.com>
2019-02-10 08:35:19 +0100

    Fixed a bug where smiley selector would appear under the who's online gradient.

--- Forum/Constants.cs
+++ Forum/Constants.cs
@@ -2,7 +2,7 @@
 	public class Constants {
 		public class Versions {
 			public const string Theme = "12";
-			public const string Code = "4.24.0";
+			public const string Code = "4.24.1";
 		}
 
 		public class InternalKeys {
