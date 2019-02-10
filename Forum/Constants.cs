500347a3621c37a0bf404efe7c8b643cee3f5f6d
Jim Yarbro <yarbro@outlook.com>
2019-02-10 08:56:37 +0100

    The root folder was intimidating and confusing for new people. I reorganized files to make things easier to contextualize.

--- Forum/Constants.cs
+++ Forum/Constants.cs
@@ -2,7 +2,7 @@
 	public class Constants {
 		public class Versions {
 			public const string Theme = "12";
-			public const string Code = "4.24.1";
+			public const string Code = "4.25.0";
 		}
 
 		public class InternalKeys {
