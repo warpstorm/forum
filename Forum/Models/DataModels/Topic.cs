ad6b10137f5f25dfdbf5d1bdd00605dcd8d11dab
Jim Yarbro <yarbro@outlook.com>
2019-03-30 20:35:57 +0100

    Added a stub for topic datamodel

---
+++ Forum/Models/DataModels/Topic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Forum.Models.DataModels {
+	public class Topic {
+		public int Id { get; set; }
+		public string Subject { get; set; }
+		public DateTime TimePosted { get; set; }
+		public string PostedById { get; set; }
+		public int ViewCount { get; set; }
+		public int ReplyCount { get; set; }
+		public bool Pinned { get; set; }
+		public bool Deleted { get; set; }
+
+		public int FirstMessageId { get; set; }
+		public int LastMessageId { get; set; }
+	}
+}
