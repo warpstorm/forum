cc1ac832b1b5c2794e0a1f45add7d0e257e45698
Jim Yarbro <yarbro@outlook.com>
2019-03-30 19:33:29 +0100

    Added some stubs for event details management

---
+++ Forum/Models/ViewModels/Messages/EventDetails.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Forum.Models.ViewModels.Messages {
+	public class EventDetails {
+		public List<SelectListItem> Years { get; set; }
+		public int StartYear { get; set; }
+		public int EndYear { get; set; }
+
+		public List<SelectListItem> Months { get; set; }
+		public int StartMonth { get; set; }
+		public int EndMonth { get; set; }
+
+		public List<SelectListItem> Days { get; set; }
+		public int StartDay { get; set; }
+		public int EndDay { get; set; }
+
+		public List<SelectListItem> Hours { get; set; }
+		public int StartHour { get; set; }
+		public int EndHour { get; set; }
+
+		public List<SelectListItem> Minutes { get; set; }
+		public int StartMinute { get; set; }
+		public int EndMinute { get; set; }
+
+		public List<SelectListItem> AmPm { get; set; }
+		public bool StartAfternoon { get; set; }
+		public bool EndAfternoon { get; set; }
+
+		public bool AllDay { get; set; }
+	}
+}
