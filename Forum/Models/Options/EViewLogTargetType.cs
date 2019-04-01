0b941b175dd53cf4dadea134a9143151232a058d
Jim Yarbro <yarbro@outlook.com>
2019-04-01 11:20:07 +0200

    Readded message target-type for now. Will remove post-migration

--- Forum/Models/Options/EViewLogTargetType.cs
+++ Forum/Models/Options/EViewLogTargetType.cs
@@ -1,6 +1,7 @@
 ﻿namespace Forum.Models.Options {
 	public enum EViewLogTargetType {
 		All,				// mark all topics read for the entire site
+		Message,			// mark single message as read
 		Topic				// mark single topic as read
 	}
 }
