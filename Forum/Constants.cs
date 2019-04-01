efb5b9d4dbb208d06f02bd8ff53173b354e0a87e
Jim Yarbro <yarbro@outlook.com>
2019-04-01 10:49:23 +0200

    We're going to version 5!

--- Forum/Constants.cs
+++ Forum/Constants.cs
@@ -2,7 +2,7 @@
 	public class Constants {
 		public class Versions {
 			public const string Theme = "12";
-			public const string Code = "4.26.4";
+			public const string Code = "5.0.0";
 		}
 
 		public class InternalKeys {
