2ca6ca74cc35c910863972c45b5cb77e42d863fe
Jim Yarbro <yarbro@outlook.com>
2019-04-02 15:54:17 +0200

    helper extension

---
+++ Forum/Extensions/AddModelErrorsExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+
+namespace Forum.Extensions {
+	public static class AddModelErrorsExtension {
+		public static void AddModelErrors(this ModelStateDictionary modelState, Dictionary<string, string> errors, string prefix = "") {
+			foreach (var error in errors) {
+				var key = prefix + error.Key;
+				modelState.AddModelError(key, error.Value);
+			}
+		}
+	}
+}
