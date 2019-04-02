85cbb2470457aa03b468799888b24f0d95381437
Jim Yarbro <yarbro@outlook.com>
2019-04-02 15:55:38 +0200

    Made properties settable for future change to how XHR forms work.

--- Forum/Models/ViewModels/Messages/EditMessageForm.cs
+++ Forum/Models/ViewModels/Messages/EditMessageForm.cs
@@ -4,7 +4,7 @@
 		public string TopicId { get; } = string.Empty;
 		public string BoardId { get; } = string.Empty;
 		public string Body { get; set; }
-		public string FormAction { get; } = nameof(Controllers.Messages.Edit);
+		public string FormAction { get; set; } = nameof(Controllers.Messages.Edit);
 		public string FormController { get; } = nameof(Controllers.Messages);
 		public string ElementId { get; set; }
 	}
