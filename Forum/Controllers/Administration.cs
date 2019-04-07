bdb0cd970e7e5b4893c44a53943a7782f796b83a
Jim Yarbro <yarbro@outlook.com>
2019-04-07 11:35:30 +0200

    Split the MigrateTopics method into two. Updated how Participants and TopicBoards are migrated. Removed legacy migration code.

--- Forum/Controllers/Administration.cs
+++ Forum/Controllers/Administration.cs
@@ -107,8 +107,8 @@ namespace Forum.Controllers {
 				var totalPages = Convert.ToInt32(Math.Floor(1d * totalRecords / take));
 
 				return Ok(new ControllerModels.Administration.Step {
-					ActionName = "Migrate Messages",
-					ActionNote = "Reprocessing message contents.",
+					ActionName = "Reprocessing Messages",
+					ActionNote = "Message contents are rebuilt, links re-checked, BBC reprocessed.",
 					Take = take,
 					TotalPages = totalPages,
 					TotalRecords = totalRecords,
@@ -260,13 +260,49 @@ namespace Forum.Controllers {
 		public async Task<IActionResult> MigrateV5() {
 			return await ForumViewResult.ViewResult(this, "MultiStep", new List<string> {
 				Url.Action(nameof(CleanupDeletedMessages)),
-				Url.Action(nameof(ReprocessMessages)),
+				Url.Action(nameof(ResetMessageTopicId)),
 				Url.Action(nameof(MigrateTopics)),
+				Url.Action(nameof(MigrateMessages)),
 				Url.Action(nameof(MigrateViewLogs)),
 				Url.Action(nameof(MigrateBookmarks)),
 				Url.Action(nameof(MigrateParticipants)),
 				Url.Action(nameof(MigrateTopicBoards)),
+				Url.Action(nameof(ReprocessMessages)),
 			});
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> ResetMessageTopicId(ControllerModels.Administration.Page input) {
+			if (input.CurrentPage < 0) {
+				var take = 100;
+				var totalRecords = await DbContext.Messages.CountAsync();
+				var totalPages = Convert.ToInt32(Math.Floor(1d * totalRecords / take));
+
+				return Ok(new ControllerModels.Administration.Step {
+					ActionName = "Reset TopicId Column",
+					ActionNote = "Resetting TopicId column on all messages.",
+					Take = take,
+					TotalPages = totalPages,
+					TotalRecords = totalRecords,
+				});
+			}
+
+			var pSkip = new SqlParameter("@Skip", input.CurrentPage * input.Take);
+			var pTake = new SqlParameter("@Take", input.Take);
+
+			await DbContext.Database.ExecuteSqlCommandAsync($@"
+					UPDATE [{nameof(ApplicationDbContext.Messages)}]
+					SET {nameof(DataModels.Message.TopicId)} = 0
+					WHERE {nameof(DataModels.Message.Id)}
+					IN (
+						SELECT Id
+						FROM [{nameof(ApplicationDbContext.Messages)}]
+						ORDER BY Id
+						OFFSET @Skip ROWS
+						FETCH NEXT @Take ROWS ONLY
+					)", pSkip, pTake);
+
+			return Ok();
 		}
 
 		[HttpPost]
@@ -276,61 +312,72 @@ namespace Forum.Controllers {
 									  select message;
 
 			if (input.CurrentPage < 0) {
-				var take = 1;
+				var take = 100;
 				var totalRecords = await parentMessagesQuery.CountAsync();
 				var totalPages = Convert.ToInt32(Math.Floor(1d * totalRecords / take));
 
 				return Ok(new ControllerModels.Administration.Step {
 					ActionName = "Migrate Topics",
-					ActionNote = "Creating topics from top level messages and assigning topic ids to all messages.",
+					ActionNote = "Creating topics from top level messages.",
 					Take = take,
 					TotalPages = totalPages,
 					TotalRecords = totalRecords,
 				});
 			}
 
-			var parentMessages = await parentMessagesQuery.Skip(input.CurrentPage * input.Take).Take(input.Take).ToListAsync();
+			var records = await parentMessagesQuery.Skip(input.CurrentPage * input.Take).Take(input.Take).ToListAsync();
 
-			foreach (var firstMessage in parentMessages) {
+			foreach (var record in records) {
 				DataModels.Message lastMessage = null;
 
-				if (firstMessage.LastReplyId > 0) {
-					lastMessage = DbContext.Messages.Find(firstMessage.LastReplyId);
+				if (record.LastReplyId > 0) {
+					lastMessage = DbContext.Messages.Find(record.LastReplyId);
 				}
 
-				var topic = new DataModels.Topic {
-					FirstMessageId = firstMessage.Id,
-					FirstMessagePostedById = firstMessage.PostedById,
-					FirstMessageTimePosted = firstMessage.TimePosted,
-					FirstMessageShortPreview = firstMessage.ShortPreview,
-					LastMessageId = lastMessage?.Id ?? firstMessage.Id,
-					LastMessagePostedById = lastMessage?.PostedById ?? firstMessage.PostedById,
-					LastMessageTimePosted = lastMessage?.TimePosted ?? firstMessage.TimePosted,
-					LastMessageShortPreview = lastMessage?.ShortPreview ?? firstMessage.ShortPreview,
-					Pinned = firstMessage.Pinned,
-					Deleted = firstMessage.Deleted,
-					ReplyCount = firstMessage.ReplyCount,
-					ViewCount = firstMessage.ViewCount
-				};
-
-				DbContext.Topics.Add(topic);
+				DbContext.Topics.Add(new DataModels.Topic {
+					FirstMessageId = record.Id,
+					FirstMessagePostedById = record.PostedById,
+					FirstMessageTimePosted = record.TimePosted,
+					FirstMessageShortPreview = record.ShortPreview,
+					LastMessageId = lastMessage?.Id ?? record.Id,
+					LastMessagePostedById = lastMessage?.PostedById ?? record.PostedById,
+					LastMessageTimePosted = lastMessage?.TimePosted ?? record.TimePosted,
+					LastMessageShortPreview = lastMessage?.ShortPreview ?? record.ShortPreview,
+					Pinned = record.Pinned,
+					Deleted = record.Deleted,
+					ReplyCount = record.ReplyCount,
+					ViewCount = record.ViewCount
+				});
 
 				await DbContext.SaveChangesAsync();
-
-				var pTopicId = new SqlParameter("@TopicId", topic.Id);
-				var pFirstMessageId = new SqlParameter("@FirstMessageId", topic.FirstMessageId);
-
-				await DbContext.Database.ExecuteSqlCommandAsync($@"
-					UPDATE [{nameof(ApplicationDbContext.Messages)}]
-					SET {nameof(DataModels.Message.TopicId)} = @TopicId
-					WHERE {nameof(DataModels.Message.Id)}
-					IN (
-						SELECT Id
-						FROM [{nameof(ApplicationDbContext.Messages)}]
-						WHERE Id = @FirstMessageId
-						OR ParentId = @FirstMessageId
-					)", pTopicId, pFirstMessageId);
 			}
+
+			return Ok();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> MigrateMessages(ControllerModels.Administration.Page input) {
+			if (input.CurrentPage < 0) {
+				var take = 100;
+				var totalRecords = await DbContext.Messages.CountAsync();
+				var totalPages = Convert.ToInt32(Math.Floor(1d * totalRecords / take));
+
+				return Ok(new ControllerModels.Administration.Step {
+					ActionName = "Migrate Messages",
+					ActionNote = "Associate messages to topics.",
+					Take = take,
+					TotalPages = totalPages,
+					TotalRecords = totalRecords,
+				});
+			}
+
+			var records = await DbContext.Messages.Skip(input.CurrentPage * input.Take).Take(input.Take).ToListAsync();
+
+			foreach (var record in records) {
+				record.TopicId = DbContext.Topics.First(item => record.ParentId == 0 ? item.FirstMessageId == record.Id : item.FirstMessageId == record.ParentId).Id;
+			}
+
+			await DbContext.SaveChangesAsync();
 
 			return Ok();
 		}
@@ -416,8 +463,8 @@ namespace Forum.Controllers {
 		[HttpPost]
 		public async Task<IActionResult> MigrateParticipants(ControllerModels.Administration.Page input) {
 			if (input.CurrentPage < 0) {
-				var take = 1;
-				var totalRecords = DbContext.Topics.Count();
+				var take = 100;
+				var totalRecords = DbContext.Participants.Count();
 				var totalPages = Convert.ToInt32(Math.Floor(1d * totalRecords / take));
 
 				return Ok(new ControllerModels.Administration.Step {
@@ -429,23 +476,20 @@ namespace Forum.Controllers {
 				});
 			}
 
-			var topics = DbContext.Topics.Skip(input.CurrentPage * input.Take).Take(input.Take);
+			var records = await DbContext.Participants.Skip(input.CurrentPage * input.Take).Take(input.Take).ToListAsync();
 
-			foreach (var topic in topics) {
-				var pTopicId = new SqlParameter("@TopicId", topic.Id);
-				var pFirstMessageId = new SqlParameter("@FirstMessageId", topic.FirstMessageId);
+			foreach (var record in records) {
+				var message = await DbContext.Messages.FirstOrDefaultAsync(item => item.Id == record.MessageId);
 
-				await DbContext.Database.ExecuteSqlCommandAsync($@"
-					UPDATE [{nameof(ApplicationDbContext.Participants)}]
-					SET {nameof(DataModels.Participant.TopicId)} = @TopicId
-					WHERE {nameof(DataModels.Participant.MessageId)}
-					IN (
-						SELECT Id
-						FROM [{nameof(ApplicationDbContext.Messages)}]
-						WHERE Id = @FirstMessageId
-						OR ParentId = @FirstMessageId
-					)", pTopicId, pFirstMessageId);
+				if (message is null) {
+					DbContext.Participants.Remove(record);
+				}
+				else {
+					record.TopicId = message.TopicId;
+				}
 			}
+
+			await DbContext.SaveChangesAsync();
 
 			return Ok();
 		}
@@ -466,17 +510,11 @@ namespace Forum.Controllers {
 				});
 			}
 
-			var topics = DbContext.Topics.Skip(input.CurrentPage * input.Take).Take(input.Take);
+			var topics = await DbContext.Topics.Skip(input.CurrentPage * input.Take).Take(input.Take).ToListAsync();
 
-			foreach (var topic in DbContext.Topics) {
-				var messagesQuery = from message in DbContext.Messages
-									where message.Id == topic.FirstMessageId || message.ParentId == topic.FirstMessageId
-									select message.Id;
-
-				var messageIds = await messagesQuery.ToListAsync();
-
+			foreach (var topic in topics) {
 				var topicBoardsQuery = from topicBoard in DbContext.TopicBoards
-									   where messageIds.Contains(topicBoard.MessageId)
+									   where topicBoard.MessageId == topic.FirstMessageId
 									   select topicBoard;
 
 				var topicBoards = await topicBoardsQuery.ToListAsync();
@@ -496,32 +534,12 @@ namespace Forum.Controllers {
 				}
 
 				DbContext.RemoveRange(topicBoards);
-				await DbContext.SaveChangesAsync();
-
 				DbContext.AddRange(newTopicBoards);
-				await DbContext.SaveChangesAsync();
 			}
 
+			await DbContext.SaveChangesAsync();
+
 			return Ok();
-		}
-
-		async Task UpdateMessageArtifacts(int topicId, int parentMessageId) {
-			var messagesQuery = from message in DbContext.Messages
-								where message.Id == parentMessageId || message.ParentId == parentMessageId
-								select message.Id;
-
-			var messageIds = await messagesQuery.ToListAsync();
-			var messageIdsString = $"{string.Join(", ", messageIds)}";
-
-			var pTopicId = new SqlParameter("@TopicId", topicId);
-			var pViewLogTypeMessage = new SqlParameter("@ViewLogTypeMessage", EViewLogTargetType.Message);
-			var pViewLogTypeTopic = new SqlParameter("@ViewLogTypeTopic", EViewLogTargetType.Topic);
-
-			await DbContext.Database.ExecuteSqlCommandAsync($"UPDATE [{nameof(ApplicationDbContext.ViewLogs)}] SET {nameof(DataModels.ViewLog.TargetId)} = @TopicId, {nameof(DataModels.ViewLog.TargetType)} = @ViewLogTypeTopic WHERE {nameof(DataModels.ViewLog.TargetType)} = @ViewLogTypeMessage AND {nameof(DataModels.ViewLog.TargetId)} IN ({messageIdsString})", pTopicId, pViewLogTypeTopic, pViewLogTypeMessage);
-			await DbContext.Database.ExecuteSqlCommandAsync($"UPDATE [{nameof(ApplicationDbContext.TopicBoards)}] SET {nameof(DataModels.TopicBoard.TopicId)} = @TopicId WHERE {nameof(DataModels.TopicBoard.MessageId)} IN ({messageIdsString})", pTopicId);
-			await DbContext.Database.ExecuteSqlCommandAsync($"UPDATE [{nameof(ApplicationDbContext.Participants)}] SET {nameof(DataModels.Participant.TopicId)} = @TopicId WHERE {nameof(DataModels.Participant.MessageId)} IN ({messageIdsString})", pTopicId);
-			await DbContext.Database.ExecuteSqlCommandAsync($"UPDATE [{nameof(ApplicationDbContext.Bookmarks)}] SET {nameof(DataModels.Bookmark.TopicId)} = @TopicId WHERE {nameof(DataModels.Bookmark.MessageId)} IN ({messageIdsString})", pTopicId);
-			await DbContext.Database.ExecuteSqlCommandAsync($"UPDATE [{nameof(ApplicationDbContext.Messages)}] SET {nameof(DataModels.Message.TopicId)} = @TopicId WHERE {nameof(DataModels.Message.Id)} IN ({messageIdsString})", pTopicId);
 		}
 
 		void CheckInstallContext() {
