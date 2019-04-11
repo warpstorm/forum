c0edf314435a8d76313d044ad88a58ee6c70c4ba
Jim Yarbro <yarbro@outlook.com>
2019-04-11 10:48:26 +0200

    deprecated class

--- Forum/Models/InputModels/Continue.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Forum.Models.InputModels {
-	public class Continue {
-		public string Stage { get; set; }
-		public int TotalSteps { get; set; }
-		public int CurrentStep { get; set; }
-	}
-}
