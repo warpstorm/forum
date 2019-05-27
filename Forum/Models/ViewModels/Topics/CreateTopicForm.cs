cce08b0af61f125b6b8008c0c8070a609bfd0074
Jim Yarbro <yarbro@outlook.com>
2019-05-27 09:44:20 +0200

    Replaced the MessageForm partial reference for the create message form. Started to link in the event management functionality a little.

--- Forum/Models/ViewModels/Topics/CreateTopicForm.cs
+++ Forum/Models/ViewModels/Topics/CreateTopicForm.cs
@@ -1,11 +1,6 @@
 ﻿namespace Forum.Models.ViewModels.Topics {
-	public class CreateTopicForm : IMessageFormViewModel {
-		public string Id { get; } = string.Empty;
-		public string TopicId { get; } = string.Empty;
+	public class CreateTopicForm {
 		public string BoardId { get; set; }
 		public string Body { get; set; }
-		public string FormAction { get; } = nameof(Controllers.Topics.Create);
-		public string FormController { get; } = nameof(Controllers.Topics);
-		public string ElementId { get; } = "create-topic";
 	}
 }
