08408da91ad7b15e493f314c39e3278c7c8824f3
Jim Yarbro <yarbro@outlook.com>
2019-05-28 12:02:29 +0200

    Added MustBeAfterAttribute for validation.

---
+++ Forum/Models/Annotations/MustBeAfterAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Forum.Models.Annotations {
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+	public class MustBeAfterAttribute : ValidationAttribute {
+		string Target { get; }
+
+		public MustBeAfterAttribute(string target) => Target = target;
+
+		protected override ValidationResult IsValid(object value, ValidationContext context) {
+			ErrorMessage = ErrorMessageString;
+
+			var thisValue = (DateTime)value;
+
+			var property = context.ObjectType.GetProperty(Target);
+
+			if (property is null) {
+				throw new ArgumentException("Property with this name not found");
+			}
+
+			var thatValue = (DateTime)property.GetValue(context.ObjectInstance);
+
+			if (thisValue < thatValue) {
+				return new ValidationResult(ErrorMessage);
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
--- Forum/Models/ControllerModels/Topics/EditEventInput.cs
+++ Forum/Models/ControllerModels/Topics/EditEventInput.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Forum.Models.Annotations;
+using System;
 
 namespace Forum.Models.ControllerModels.Topics {
 	public class EditEventInput {
 		public DateTime? Start { get; set; }
+
+		[MustBeAfter(nameof(Start))]
 		public DateTime? End { get; set; }
+
 		public bool AllDay { get; set; }
+
 		public int TopicId { get; set; } = -1;
 		public string Body { get; set; }
 		public string SelectedBoards { get; set; }
