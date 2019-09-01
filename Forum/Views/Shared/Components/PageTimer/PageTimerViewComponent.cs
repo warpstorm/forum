eae45c91bb7857314271a3f0c3ee30d7124c6fbe
Jim Yarbro <yarbro@outlook.com>
2019-09-01 08:13:23 +0200

    Added page timer view component

---
+++ Forum/Views/Shared/Components/PageTimer/PageTimerViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Forum.Views.Shared.Components.SmileySelector {
+	public class PageTimerViewComponent : ViewComponent {
+		public IViewComponentResult Invoke() {
+			var timerText = string.Empty;
+
+			if (false && HttpContext.Items["PageTimer"] is Stopwatch pageTimer) {
+				pageTimer.Stop();
+				var pageTimerSeconds = 1D * pageTimer.ElapsedMilliseconds / 1000;
+				timerText = $"Loaded in {pageTimerSeconds} seconds";
+			}
+
+			return View("Default", timerText);
+		}
+	}
+}
