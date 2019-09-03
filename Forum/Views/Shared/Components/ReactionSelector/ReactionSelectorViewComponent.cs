27c096c086f77b1a66b2bfa816c2f9ee59b21116
Jim Yarbro <yarbro@outlook.com>
2019-09-03 11:04:18 +0200

    Fixed a bug when an imgur account is linked but has no favorites. Added notes to help people get favorites.

--- Forum/Views/Shared/Components/ReactionSelector/ReactionSelectorViewComponent.cs
+++ Forum/Views/Shared/Components/ReactionSelector/ReactionSelectorViewComponent.cs
@@ -23,39 +23,37 @@ namespace Forum.Views.Shared.Components.ReactionSelector {
 		}
 
 		public async Task<IViewComponentResult> InvokeAsync() {
-			if (UserContext.Imgur is null) {
-				return View("Default", default(ReactionSelectorViewModel));
-			}
+			var viewModel = new ReactionSelectorViewModel();
 
-			if (UserContext.Imgur.AccessTokenExpiration < DateTime.Now) {
-				await ImgurClient.RefreshToken();
-			}
-
-			if (!UserContext.Imgur.Favorites?.Any() ?? false || UserContext.Imgur.FavoritesUpdate < DateTime.Now.AddHours(-1)) {
-				var favorites = await ImgurClient.GetFavorites();
-
-				if (!favorites.Any()) {
-					return View("Default", default(ReactionSelectorViewModel));
+			if (!(UserContext.Imgur is null)) {
+				if (UserContext.Imgur.AccessTokenExpiration < DateTime.Now) {
+					await ImgurClient.RefreshToken();
 				}
 
-				UserContext.Imgur.Favorites = favorites;
-				UserContext.Imgur.FavoritesUpdate = DateTime.Now;
-				DbContext.Update(UserContext.Imgur);
-				await DbContext.SaveChangesAsync();
+				if (!UserContext.Imgur.Favorites?.Any() ?? false || UserContext.Imgur.FavoritesUpdate < DateTime.Now.AddHours(-1)) {
+					var favorites = await ImgurClient.GetFavorites();
+
+					if (favorites.Any()) {
+						UserContext.Imgur.Favorites = favorites;
+						UserContext.Imgur.FavoritesUpdate = DateTime.Now;
+						DbContext.Update(UserContext.Imgur);
+						await DbContext.SaveChangesAsync();
+					}
+				}
+
+				var images = new List<ReactionSelectorItem>();
+
+				if (!(UserContext.Imgur.Favorites is null)) {
+					foreach (var item in UserContext.Imgur.Favorites) {
+						images.Add(new ReactionSelectorItem {
+							Id = item,
+							Path = $"https://i.imgur.com/{item}.mp4"
+						});
+					}
+				}
+
+				viewModel.Images = images;
 			}
-
-			var images = new List<ReactionSelectorItem>();
-
-			foreach (var item in UserContext.Imgur.Favorites) {
-				images.Add(new ReactionSelectorItem {
-					Id = item,
-					Path = $"https://i.imgur.com/{item}.mp4"
-				});
-			}
-
-			var viewModel = new ReactionSelectorViewModel {
-				Images = images
-			};
 
 			return View("Default", viewModel);
 		}
